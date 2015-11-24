2b95f47724dd19167f145780cd0442865b6bea7d
Brian Duggan <bduggan3387@gmail.com>
2015-11-23 20:04:10 -0500

    adds functioning logic to play game

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,58 @@
-console.log("...loaded");
+// GAME STATE & LOGIC
 
-// ♠  ♣  ♥  ♦
+// DISPLAY
 
+// INTERACTION
 var $playerScore = $('#player-score'),
-    $computerScore = $('#computer-score'),
     $playerCards = $('#player-cards'),
-    $computerCards = $('#computer-cards'),
+    $dealerScore = $('#dealer-score'),
+    $dealerCards = $('#dealer-cards'),
     $hit = $('#hit'),
-    $stay = $('#stay'),
-    $result = $('#result'),
-    playerTotal,
-    computerTotal,
-    deck = ["2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","J♠","Q♠","K♠","A♠",
-            "2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","J♣","Q♣","K♣","A♣",
-            "2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","J♥","Q♥","K♥","A♥",
-            "2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","J♦","Q♦","K♦","A♦"];
+    $stand = $('#stand'),
+    $dealComp = $('#deal-computer'),
+    $result = $('#result');
+
+
+// var deck = ["2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","J♠","Q♠","K♠","A♠",
+//         "2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","J♣","Q♣","K♣","A♣",
+//         "2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","J♥","Q♥","K♥","A♥",
+//         "2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","J♦","Q♦","K♦","A♦"];
+
+var deck = [
+  {name: "2♠", value: 2, suit: "♠" }, {name: "3♠", value: 3, suit: "♠" },
+  {name: "4♠", value: 4, suit: "♠" }, {name: "5♠", value: 5, suit: "♠" },
+  {name: "6♠", value: 6, suit: "♠" }, {name: "7♠", value: 7, suit: "♠" },
+  {name: "8♠", value: 8, suit: "♠" }, {name: "9♠", value: 9, suit: "♠" },
+  {name: "10♠", value: 10, suit: "♠" }, {name: "J♠", value: 10, suit: "♠" },
+  {name: "Q♠", value: 10, suit: "♠" }, {name: "K♠", value: 10, suit: "♠" },
+  {name: "A♠", value: 11, suit: "♠", altValue: 1},
+  {name: "2♣", value: 2, suit: "♣" }, {name: "3♣", value: 3, suit: "♣" },
+  {name: "4♣", value: 4, suit: "♣" }, {name: "5♣", value: 5, suit: "♣" },
+  {name: "6♣", value: 6, suit: "♣" }, {name: "7♣", value: 7, suit: "♣" },
+  {name: "8♣", value: 8, suit: "♣" }, {name: "9♣", value: 9, suit: "♣" },
+  {name: "10♣", value: 10, suit: "♣" }, {name: "J♣", value: 10, suit: "♣" },
+  {name: "Q♣", value: 10, suit: "♣" }, {name: "K♣", value: 10, suit: "♣" },
+  {name: "A♣", value: 11, suit: "♣", altValue: 1},
+  {name: "2♥", value: 2, suit: "♥" }, {name: "3♥", value: 3, suit: "♥" },
+  {name: "4♥", value: 4, suit: "♥" }, {name: "5♥", value: 5, suit: "♥" },
+  {name: "6♥", value: 6, suit: "♥" }, {name: "7♥", value: 7, suit: "♥" },
+  {name: "8♥", value: 8, suit: "♥" }, {name: "9♥", value: 9, suit: "♥" },
+  {name: "10♥", value: 10, suit: "♥" }, {name: "J♥", value: 10, suit: "♥" },
+  {name: "Q♥", value: 10, suit: "♥" }, {name: "K♥", value: 10, suit: "♥" },
+  {name: "A♥", value: 11, suit: "♥", altValue: 1},
+  {name: "2♦", value: 2, suit: "♦" }, {name: "3♦", value: 3, suit: "♦" },
+  {name: "4♦", value: 4, suit: "♦" }, {name: "5♦", value: 5, suit: "♦" },
+  {name: "6♦", value: 6, suit: "♦" }, {name: "7♦", value: 7, suit: "♦" },
+  {name: "8♦", value: 8, suit: "♦" }, {name: "9♦", value: 9, suit: "♦" },
+  {name: "10♦", value: 10, suit: "♦" }, {name: "J♦", value: 10, suit: "♦" },
+  {name: "Q♦", value: 10, suit: "♦" }, {name: "K♦", value: 10, suit: "♦" },
+  {name: "A♦", value: 11, suit: "♦", altValue: 1}
+];
+
 
 $hit.hide();
-$stay.hide();
+$stand.hide();
+$dealComp.hide();
 
 //////// DRAW RANDOM CARD ////////
 
@@ -37,23 +72,25 @@ function drawCard(){
   return result;
 } //////// END DRAW CARD ////////
 
-//////// PARSING FUNCTIONS ////////
-function parseCard(card){
-  var cardNum = card.substring(0,1);
-  return cardNum;
-}
-
-function parseSymbol(card){
-  var cardSym = card.substring(1,2);
-  return cardSym;
-}
-//////// END PARSING FUNCTIONS ////////
+// //////// PARSING FUNCTIONS ////////
+// function parseCard(card){
+//   if (typeof card === 'string'){
+//     var cardNum = card.substring(0,1);
+//     return cardNum;
+//   }
+// }
+//
+// function parseSymbol(card){
+//   var cardSym = card.substring(1,2);
+//   return cardSym;
+// }
+// //////// END PARSING FUNCTIONS ////////
 
 //////// CONTAINS AN ACE? ////////
 function containsAce(hand){
   var aceCount = 0;
   for (var i = 0; i < hand.length; i++) {
-    if (parseCard(hand[i]) === "A"){
+    if (hand[i].value === 11){
       aceCount += 1;
     } // end if
   } // end for
@@ -62,220 +99,174 @@ function containsAce(hand){
 }
 //////// END CONTAINS ACE ////////
 
-//////// DISPLAY HAND FUNCTION ////////
-function displayHand(cards, player){
-  var cardArray = cards;
-  var currentPlayer = player;
-  var divAppend;
-
-  if (typeof cardArray === 'string'){
-    cardArray = []
-    cardArray.push(cards)
-  }
-  if (currentPlayer === "player"){
-    divAppend = $playerCards;
-  } else {
-    divAppend = $computerCards;
-  }
-  for (var i = 0; i < cardArray.length; i++){
-    var $span;
-    if (cardArray[i] === "X"){
-      $span = $('<span>').text(" X ");
-      divAppend.append($span);
-    } else if (parseSymbol(cardArray[i]) === "♥" || parseSymbol(cardArray[i]) === "♦"){
-      $span = $('<span>').css("color", "red");
-      $span.append(cardArray[i]);
-      divAppend.append($span);
-    } else {
-      $span = $('<span>').css("color", "black");
-      $span.append(cardArray[i]);
-      divAppend.append($span);
-    } // end if
-  } // end for
-
-} //////// END DISPLAY HAND FUNCTION ////////
-
-//////// DISPLAY SCORE FUNCTION ////////
-function displayScore(total, player){
-
-  if (player === "player"){
-    $playerScore.text("Player has " + total);
-  } else {
-    $computerScore.text("Computer has " + total);
-  }
-
-
-} //////// END DISPLAY SCORE FUNCTION ////////
-
-//////// FIND HAND TOTALS ////////
-function handTotal(hand){
-  var handArray = hand;
-  var handTotal = 0;
-  var aces = containsAce(hand);
-
-  for (var i = 0; i < hand.length; i++) {
-    if (parseCard(hand[i]) === "A"){
-      hand[i] = 11;
-    } else if (parseCard(hand[i]) === "K"|| parseCard(hand[i]) === "Q" || parseCard(hand[i]) === "J"){
-      hand[i] = 10;
-    } else {
-      hand[i] = parseInt(hand[i]);
-    } // end if
-  } // end for
-
-  for (var n = 0; n <handArray.length; n++){
-    handTotal += handArray[n];
-  }
-
-  if (handTotal > 21 && aces > 0) {
-    handTotal = ( (handTotal - (aces*11) ) + aces);
-  }
-
-  return handTotal;
-
-} //////// END HAND TOTAL FUNCTION ////////
-
-
-function roundOne(){
-  var computer1 = drawCard(),
-      computer2 = drawCard(),
-      player1 = drawCard(),
-      player2 = drawCard(),
-      computerHand = [computer1, computer2],
-      playerHand = [player1, player2];
-  computerTotal = handTotal(computerHand);
-  playerTotal = handTotal(playerHand);
-
-  displayHand([player1, player2], "player");
-  displayHand(["X", computer2], "dealer");
-  console.log("The Computer draws " + computer1 + " and " + computer2);
-  console.log("The Player draws " + player1 + " and " + player2);
-
-  displayScore(computerTotal, "computer");
-  displayScore(playerTotal, "player")
-  return[ [player1,player2], playerTotal, [computer1,computer2], computerTotal ];
-} ///// END START GAME FUNCTION /////
-
-function playerBust(total){
-  var player = total;
-  console.log(player);
-  if (player > 21) {
-    console.log("hello!");
-    $result.text("Player Busts!!!");
-    return total;
-  } else if (player === 21){
-    $result.text("Blackjack");
-  } else {
-    $hit.show();
-    $stay.show();
-    return total;
-  }
-};
-
-
-//////// SHOULD PLAYER HIT OR STAND ////////
-
-function playerMove(array){
-  var oldPlayerHand = array[0];
-  var player = array[1];
-
-  if (player > 20){
-    $hit.hide();
-    $stay.hide();
-    return [oldPlayerHand];
-  } else if (player < 21){
-    $hit.show();
-    $stay.show();
-    $hit.on('click',function (){
-      var newCard = drawCard();
-      displayHand(newCard, "player");
-      oldPlayerHand.push(newCard);
-      player = handTotal(oldPlayerHand);
-      displayScore(player,"player");
-      $hit.hide();
-      $stay.hide();
-    });
-
-    $stay.on('click',function (){
-      $hit.hide();
-      $stay.hide();
-      return [oldPlayerHand];
-    });
-  }
-
-} //////// END PLAYER HIT STAND FUNCTION ///////
-
-
-
-//////// SHOULD DEALER STAND ////////
-function dealerStand (array){
-  var dealer = handTotal(array[1]);
-  if (dealer > 21) {
-    return "Bust";
-  } else if (dealer === 21){
-    return "Blackjack";
-  } else if (dealer < 21 && dealer > 16){
-    return "Stand";
-  } else {
-    return "Hit";
-  } // end if
-} //////// END DEALER STAND FUNCTION ////////
-
-
-///////////////////////////
-// function testWin (array){
-//   var dealerHand = array[1];
-//   var playerHand = array[0];
-//   var output = 0;
-//
-//   if (dealerHand === playerHand && dealerHand === 21) {
-//         console.log("Two Blackjacks! Push!");
-//         return "Push"
-//   }  else if (playerHand === 21){
-//         console.log("You Win!");
-//         return "Player"
-//   }  else if (dealerHand === 21){
-//         console.log("Blackjack! Dealer Wins!");
-//         return "Computer"
-//   } else if (dealerHand > 16 && playerHand > dealerHand){
-//         console.log("You Win!");
-//   } else if (dealerHand > 16){
-//         console.log("Dealer Stands");
-//   } else {
-//         console.log("Dealer Hits!");
-//   }
-//
-//   //pseudo-code--- set console.logs to dealerOutput, then insert DealerOutput into DOM visually
-//   //               then...
-//
-//
-// }
-
-/*
-
-Idea... until player total is over 20 show hit and stand... press stand and hide hit, stand
-
-while player < 20 ||¥
-
-
-other idea, set span equal to player total, do a while loop on $.text() === of it
-
-
-*/
-
-
-function startGame(){
-
-  var firstRound = roundOne();
-  console.log(playerTotal);
-  var playerStart = playerMove(firstRound);
-
+//////// CREATE BLACKJACK GAME MODEL ////////
+function Blackjack (){
+  this.dealer1 = drawCard();
+  this.dealer2 = drawCard();
+  this.player1 = drawCard();
+  this.player2 = drawCard();
+  this.gameState = [[this.player1, this.player2],[this.dealer1,this.dealer2]];
+  this.stand = false;
+  this.handTotal = 0;
 }
 
+// Blackjack.prototype.hit = function (){
+//   $hit.show();
+//   $hit.hide();
+//   var playerHit = drawCard();
+//   this.gameState[0].push(playerHit);
+//   this.render();
+//   if (this.playerTotal() > 21){
+//     this.stand = true;
+//     $hit.hide();
+//     $stand.hide();
+//   };
+// };
+//
+// Blackjack.prototype.playerStand = function (){
+//   this.stand = true;
+//   $hit.hide();
+//   $stand.hide();
+// };
 
- // DISABLE FOR TESTING PURPOSES
-$('#start').on('click', function(e){
-  e.preventDefault();
-  startGame();
-  $('#start').hide();
+Blackjack.prototype.playerTotal = function(){
+  var playerhand = this.gameState[0];
+  var handTotal = 0;
+  var aces = containsAce(playerhand);
+
+  for (var n = 0; n < playerhand.length; n++){
+    handTotal += playerhand[n].value;
+  }
+  if (handTotal > 21 && aces > 0) {
+    handTotal = ( (handTotal - (aces*11) ) + aces);
+    return handTotal;
+  } else {
+    return handTotal;
+  }
+
+};
+
+Blackjack.prototype.computerTotal = function(){
+  var playerhand = this.gameState[1];
+  var handTotal = 0;
+  var aces = containsAce(playerhand);
+
+  for (var n = 0; n < playerhand.length; n++){
+    handTotal += playerhand[n].value;
+  }
+  if (handTotal > 21 && aces > 0) {
+    handTotal = ( (handTotal - (aces*11) ) + aces);
+    return handTotal;
+  } else {
+    return handTotal;
+  }
+
+};
+
+Blackjack.prototype.render = function(){
+
+  $playerCards.empty();
+
+  $playerScore.empty();
+  $dealerScore.empty();
+
+  for (var i = 0; i < this.gameState[0].length; i++){
+    var $span;
+
+    if (this.gameState[0][i].suit === "♥" || (this.gameState[0][i].suit) === "♦"){
+      $span = $('<span>').css("color", "red");
+      $span.append(this.gameState[0][i].name);
+      $playerCards.append($span);
+    } else {
+      $span = $('<span>').css("color", "black");
+      $span.append(this.gameState[0][i].name);
+      $playerCards.append($span);
+    } // end if
+  } // end for
+
+  for (var n = 0; i < this.gameState[1].length; n++){
+    var $span;
+    if (this.gameState[1][n].value === "X"){
+      $span = $('<span>').text(" X ");
+      $dealerCards.append($span);
+  } else if (this.gameState[1][n].suit === "♥" ||  this.gameState[1][n].suit === "♦"){
+      $span = $('<span>').css("color", "red");
+      $span.append(this.gameState[1][n]);
+      $dealerCards.append($span);
+    } else {
+      $span = $('<span>').css("color", "black");
+      $span.append(this.gameState[1][i]);
+      $dealerCards.append($span);
+    } // end if
+  } // end for
+
+  $playerScore.append("Player has " + this.playerTotal());
+  $dealerScore.append("Computer has the " + this.gameState[1][1].name);
+
+};
+
+// Blackjack.prototype.addEventListeners = function(){
+//   var scope = this;
+//   $hit.on('click', scope.hit);
+//   $stand.on('click', scope.playerStand);
+//
+// };
+
+
+
+
+$(document).ready(function(){
+  var game = new Blackjack();
+  $('#start').on('click', function(){
+    $('#start').hide();
+    game.render();
+    $hit.show();
+    $stand.show();
+  });
+
+  $hit.on('click', function (){
+    $hit.show();
+
+    var playerHit = drawCard();
+    game.gameState[0].push(playerHit);
+    game.render();
+    if (game.playerTotal() > 21){
+      game.stand = true;
+      $hit.hide();
+      $stand.hide();
+      $dealComp.show();
+    };
+  });
+
+  $stand.on('click', function(){
+    game.stand = true;
+    $hit.hide();
+    $stand.hide();
+    $dealComp.show();
+  });
+
+  $dealComp.on('click', function(){
+    var dealerHand = game.gameState[1];
+    var counter = game.computerTotal()
+    while (counter < 17){
+      console.log(counter);
+      var computerHit = drawCard();
+      game.gameState[1].push(computerHit);
+
+      counter = game.computerTotal();
+      console.log(counter);
+    }
+    $result.text("Dealer has " + game.computerTotal());
+
+  })
+
 });
+
+
+///BE ABLE TO HIT UNTIL BUST OR STAND WHICH WILL SET STAND == TRUE
+/// THEN .... IT WILL BE COMPUTERS TURN WHEN STAND == TRUE
+
+
+// CREATE INDIVIDUAL BLACKJACK GAME
+// var game = new Blackjack();
+// game();
